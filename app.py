4fa08712d65f4075d7bcd45198d223bbb8a77757
EbiScott <egbejuleeworitse@gmail.com>
2024-07-02 04:09:21 +0100

    Revert "Updated app.py"
    This reverts commit 783ef9c05a57c2a53d7d477cbe76d108b0cfdf31.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def create_app():
     app = Flask(__name__)
     client = MongoClient(os.environ.get("MONGODB_URI"))
@@ -16,6 +17,7 @@ def create_app():
         if request.method == "POST":
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y- %m- %d")
+            entries.append((entry_content, formatted_date)) 
             app.db.entries.insert({"content": entry_content, "date": formatted_date})
 
         entries_with_date = [
@@ -26,6 +28,7 @@ def create_app():
             )
             for entry in app.db.entries.find({})
         ]
+        
         return render_template("home.html", entries=entries_with_date)
     
     return app
